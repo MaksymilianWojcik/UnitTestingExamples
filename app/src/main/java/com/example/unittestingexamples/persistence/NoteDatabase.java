b76e1fa8c843ee1ae8a5fd795f620e0da72b9f63
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-09-25 21:06:32 +0200

    Fixed entity

--- app/src/main/java/com/example/unittestingexamples/persistence/NoteDatabase.java
+++ app/src/main/java/com/example/unittestingexamples/persistence/NoteDatabase.java
@@ -4,7 +4,9 @@ package com.example.unittestingexamples.persistence;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = { NoteDao.class}, version = 1)
+import com.example.unittestingexamples.models.Note;
+
+@Database(entities = { Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 
     public static final String DATABASE_NAME = "notes_db";
