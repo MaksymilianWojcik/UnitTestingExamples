2616e8e568e8ec01be8519c290127918aa3d43a5
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-09-25 14:31:58 +0200

    Added more note model tests

--- app/src/test/java/com/example/unittestingexamples/models/NoteTest.java
+++ app/src/test/java/com/example/unittestingexamples/models/NoteTest.java
@@ -15,7 +15,41 @@ public class NoteTest {
 
         Assertions.assertEquals(note1, note2);
         System.out.println("The notes are equal!");
+    }
 
+    @Test
+    void isNotesEqual_differentIds_returnFalse() throws Exception {
+        Note note1 = new Note(1, "Note #1", "This is note #1", TIMESTAMP_1);
+        Note note2 = new Note(2, "Note #1", "This is note #1", TIMESTAMP_1);
 
+        Assertions.assertNotEquals(note1, note2);
+        System.out.println("The notes are not equal!");
+    }
+
+    @Test
+    void isNotesEqual_differentTimestamps_returnTrue() throws Exception {
+        Note note1 = new Note(1, "Note #1", "This is note #1", TIMESTAMP_1);
+        Note note2 = new Note(1, "Note #1", "This is note #1", TIMESTAMP_2);
+
+        Assertions.assertEquals(note1, note2);
+        System.out.println("The notes are not equal!");
+    }
+
+    @Test
+    void isNotesEqual_differentTitles_returnFalse() throws Exception {
+        Note note1 = new Note(1, "Note #1", "This is note #1", TIMESTAMP_1);
+        Note note2 = new Note(1, "Note #2", "This is note #1", TIMESTAMP_1);
+
+        Assertions.assertNotEquals(note1, note2);
+        System.out.println("The notes are not equal!");
+    }
+
+    @Test
+    void isNotesEqual_differentContent_returnFalse() throws Exception {
+        Note note1 = new Note(1, "Note #1", "This is note #1", TIMESTAMP_1);
+        Note note2 = new Note(1, "Note #2", "This is note #2", TIMESTAMP_1);
+
+        Assertions.assertNotEquals(note1, note2);
+        System.out.println("The notes are not equal!");
     }
 }
