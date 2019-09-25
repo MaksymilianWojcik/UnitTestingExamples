cb6b4178a68f443d200cb38c9b24d8da7d3e28a4
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-09-25 20:09:27 +0200

    Setup dagger

---
+++ app/src/main/java/com/example/unittestingexamples/di/ActivityBuildersModule.java
@@ -0,0 +1,13 @@
+package com.example.unittestingexamples.di;
+
+import com.example.unittestingexamples.NotesListActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract NotesListActivity contributeNotesListActivity();
+}
