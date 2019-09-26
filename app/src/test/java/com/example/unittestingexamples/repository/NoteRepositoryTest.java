5c873bdb641da340ab5ec6eaaecf9b764a3e9904
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-09-26 15:11:44 +0200

    Added note repository tests

--- app/src/test/java/com/example/unittestingexamples/repository/NoteRepositoryTest.java
+++ app/src/test/java/com/example/unittestingexamples/repository/NoteRepositoryTest.java
@@ -1,18 +1,27 @@
 package com.example.unittestingexamples.repository;
 
+import com.example.unittestingexamples.models.Note;
 import com.example.unittestingexamples.persistence.NoteDao;
+import com.example.unittestingexamples.ui.Resource;
+import com.example.unittestingexamples.utils.TestUtil;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import io.reactivex.Single;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class NoteRepositoryTest {
+
+
+    private static final Note NOTE1 = new Note(TestUtil.TEST_NOTE_1);
 
     //system under test
     private NoteRepository noteRepository;
@@ -28,9 +37,70 @@ public class NoteRepositoryTest {
         noteRepository = new NoteRepository(noteDao);
     }
 
+    // Insert note, verify the correct method is called, confirm observer is triggered,
+    // confirm the new rows are inserted
     @Test
-    void dummyTest() throws Exception {
-        assertNotNull(noteDao);
-        assertNotNull(noteRepository);
+    void insertNote_returnRow() throws Exception {
+
+        // Arrange
+        final Long insertedRow = 1L; // succesfull response
+        final Single<Long> returnedData = Single.just(insertedRow);
+
+        // what we want mockdao to do: (so we mock here)
+        // what the insert method is called in dao, and any note object is inserted, than we want to retur
+        // so this is defining what we want mock to do when i try to insert note object into db
+        // so we define what we want to happen on the dao, which is the mock
+        when(noteDao.insertNote(any(Note.class))).thenReturn(returnedData);
+
+        // ACT
+        // this is gonna try and insert note into db
+        // if i would use LiveData instead ofrxjava like in instrumentation tets, i wouldneed to add something for allowing to test in bacgkround, likw i did in instrumentation tests
+        final Resource<Integer> returnedValue = noteRepository.insertNote(NOTE1).blockingFirst(); // blocking cause we return flowable
+
+        //  Assert
+        verify(noteDao).insertNote(any(Note.class)); // veryfing that the insertNote method was called with any Note object on dao
+        verifyNoMoreInteractions(noteDao); // it says make sure the insdertNote method was called and after that nothing
+
+        System.out.println("Returned value: " + returnedValue.data);
+        assertEquals(Resource.success(1, NoteRepository.INSERT_SUCCESS), returnedValue);
+
+
+        // or test using rxjava eexcept the whole code above
+//        noteRepository.insertNote(NOTE1)
+//                .test()
+//                .await()
+//                .assertValue(Resource.success(1, NoteRepository.INSERT_SUCCESS));
+
+    }
+
+    // Insert note, Failure (return -1)
+    @Test
+    void insertNote_returnError() throws Exception {
+
+        final Long failedInsert = -1L;
+        final Single<Long> returnedData = Single.just(failedInsert);
+
+        when(noteDao.insertNote(any(Note.class))).thenReturn(returnedData);
+        final Resource<Integer> returnedValue = noteRepository.insertNote(NOTE1).blockingFirst();
+
+        verify(noteDao).insertNote(any(Note.class));
+        verifyNoMoreInteractions(noteDao);
+
+        assertEquals(Resource.error(null, NoteRepository.INSERT_FAILURE), returnedValue);
+    }
+
+    // Insert note, null title, confirm throw exception
+    @Test
+    void insertNote_nullTitle_throwException() throws Exception {
+
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                final Note note = new Note(TestUtil.TEST_NOTE_1);
+                note.setTitle(null);
+                noteRepository.insertNote(note);
+            }
+        });
+
     }
 }
